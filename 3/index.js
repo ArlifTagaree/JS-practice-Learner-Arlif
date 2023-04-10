ca0eff585cfa16e0ed8d85772097457f348c6e9b
ArlifTagaree <liffism9@gmail.com>
2023-04-10 18:11:56 +0700

    Edit 3

--- 3/index.js
+++ 3/index.js
@@ -1,7 +1,22 @@
 function calculateTax(income) {
 
+
   // your code here
+  if(income >= 500000){
+    return console.log(`Output : ${income*0.15}`);
+  } else if (income >= 300001 && income <= 500000) {
+    return console.log(`Output : ${income*0.10}`);
+  } else if (income >= 150001 && income <= 300000) {
+    return console.log(`Output : ${income*0.05}`);
+  } else {
+    return console.log(`Output : 0`);
+  }
 
 }
+
+calculateTax(150000);
+calculateTax(220000);
+calculateTax(700000);
+
 
 module.exports = calculateTax;
