6988525f360f373e73c59f58549ee15d5d0e5798
ArlifTagaree <liffism9@gmail.com>
2023-04-10 17:45:24 +0700

    Edit 1

--- 1/index.js
+++ 1/index.js
@@ -1,8 +1,17 @@
 function ageClassification(age) {
 
     // Your code here
+    if(age <= 18){
+        return console.log('Minor')
+    } else if (age >18 && age < 65){
+        return console.log('Adult')
+    } else {
+        return console.log('Senior')
+    }
 
 }
+
+ageClassification(16)
 
 
 module.exports = ageClassification
